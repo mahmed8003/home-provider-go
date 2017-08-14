d45facbaa2d926f266f9b77341a949ffbaa2fdb5
Muhammad Ahmed <mahmed8003@gmail.com>
2017-08-15 01:50:51 +0500

    Updated user model

--- models/user.go
+++ models/user.go
@@ -4,5 +4,9 @@ import "gopkg.in/mgo.v2/bson"
 
 // User holds metadata about a user.
 type User struct {
-	Id bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
+	Id       bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string        `json:"name,omitempty" bson:"name,omitempty"`
+	Email    string        `json:"email,omitempty" bson:"email,omitempty"`
+	Mobile   string        `json:"mobile,omitempty" bson:"mobile,omitempty"`
+	Password string        `json:"password,omitempty" bson:"password,omitempty"`
 }
